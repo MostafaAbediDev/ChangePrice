e633d7e8d8a10e4bc99c620be938d04bea74cc8d
Mostafa Abedi <112893244+Mostafa-programmer@users.noreply.github.com>
2025-06-26 10:39:38 +0430

    Client Validation Implemented

--- ProductManagement.Application.Contract/Products/CreateProduct.cs
+++ ProductManagement.Application.Contract/Products/CreateProduct.cs
@@ -9,13 +9,16 @@ namespace ProductManagement.Application.Contract.Products
     {
         [FileExtentionLimitation(new string[] { ".jpeg", ".jpg", ".png" }, ErrorMessage = ValidationMessages.InValidFileFormat)]
         [MaxFileSize(3 * 1024 * 1024, ErrorMessage = ValidationMessages.MaxFileSize)]
-        public IFormFile? Picture { get; set; }
+        public IFormFile Picture { get; set; }
         public string? PictureAlt { get; set; }
         public string? PictureTitle { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
-        public string? Name { get; set; }
-        public string? UnitOfMeasurement { get; set; }
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string UnitOfMeasurement { get; set; }
+
         public long PriceInIran { get; set; }
         public int DefaultCount { get; set; }
 
